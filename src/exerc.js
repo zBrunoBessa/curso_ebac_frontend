b2cea3ff9009da56f162d8b30e2cf79db0c0ffd8
Bruno Bessa <beduardo2014@gmail.com>
2023-03-11 10:13:47 -0300

    first commit

---
+++ src/exerc.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicacao(9, 6));
+console.log(saudacao("Bruno"));
---
+++ src/exerc.ts
@@ -0,0 +1,10 @@
+function multiplicacao( n1 : number , n2 : number) {
+    return n1 * n2;
+}
+
+function saudacao(nome: string) {
+    return "Olá " + nome;
+}
+
+console.log(multiplicacao(9, 6));
+console.log(saudacao("Bruno"));
